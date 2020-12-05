f1b5437abcf2906220bac2b56fc2ca3d69ff35c7
chrismad26 <cmadison@chicagobooth.edu>
2020-12-05 22:40:21 +0000

    got feed to work

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -6,18 +6,22 @@ class UserController < ApplicationController
 
     render({ :template => "users/index.html.erb" })
   end
+
   def show
-    @the_user = User.where({ :username => params.fetch("username")}).at(0)
+    @the_user = User.where({ :username => params.fetch("username") }).at(0)
     @list_of_photos = @the_user.own_photos
 
     render({ :template => "users/show.html.erb" })
   end
+
   def feed
-    @the_user = User.where({ :username => params.fetch("username")}).at(0)
-    @users_followed =  FollowRequest.where({ :sender_id => @the_user.id}).where({ :status => "Accepted"}).all
-    # @list_of_photos = @the_user.own_photos
+    @the_user = User.where({ :username => params.fetch("username") }).at(0)
+    @requests_accepted = FollowRequest.where({ :sender_id => @the_user.id }).where({ :status => "accepted" })
+    @ids_followed = @requests_accepted.map_relation_to_array(:recipient_id)
+    @users_followed = User.where({ :id => @ids_followed })
+
+    @list_of_photos = Photo.where({ :owner_id => @ids_followed })
 
     render({ :template => "users/feed.html.erb" })
   end
-
 end
